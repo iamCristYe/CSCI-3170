b7d6786e6743f8b23d30d9afda4c52c50d86490b
zr67800 <17163263+zr67800@users.noreply.github.com>
2018-04-08 00:52:24 +0800

    basic architecture done

--- CSCI3170Proj.java
+++ CSCI3170Proj.java
@@ -61,12 +61,156 @@ public class CSCI3170Proj {
 		}	
 	}
 
-	public static void customerMenu(Scanner menuAns, Connection mySQLDB) throws SQLException{
+	public static void neaSearch(Scanner menuAns, Connection mySQLDB) throws SQLException{
+		//TODO
+		String answer = "";
+		String keyword = "";
+		while(true){
+			System.out.println();
+			System.out.println("Choose the search criterion:");
+			System.out.println("1. ID");
+			System.out.println("2. Family");
+			System.out.println("3. Resource type");
+			System.out.println("My criterion:");
+			answer = menuAns.nextLine();
+			if (answer.equals("1")||answer.equals("2")||answer.equals("3")){
+				break;
+			}else{
+				System.out.println("[Error]: Wrong Input, Type in again!!!");
+			}
+		}
+		System.out.println();
+		System.out.println("Type in the search keyword");
+		keyword = menuAns.nextLine();
+		if(answer.equals("1")){
+			;
+		}else if(answer.equals("2")){
+			;
+		}else if(answer.equals("3")){
+			;
+			//TODO
+		}
+	}
+
+	public static void spacecraftSearch(Scanner menuAns, Connection mySQLDB) throws SQLException{
+		//TODO
+		String answer = "";
+		String keyword = "";
+		while(true){
+			System.out.println();
+			System.out.println("Choose the search criterion:");
+			System.out.println("1. Agency Name");
+			System.out.println("2. Type");
+			System.out.println("3. Least energy [km/s]");
+			System.out.println("4. Least working time [days]");
+			System.out.println("5. Least capacity [m^3]");
+			System.out.println("My criterion:");
+			answer = menuAns.nextLine();
+			if (answer.equals("1")||answer.equals("2")||answer.equals("3")||answer.equals("4")||answer.equals("5")){
+				break;
+			}else{
+				System.out.println("[Error]: Wrong Input, Type in again!!!");
+			}
+		}
+		System.out.println();
+		System.out.println("Type in the search keyword");
+		keyword = menuAns.nextLine();
+		if(answer.equals("1")){
+			;
+		}else if(answer.equals("2")){
+			;
+		}else if(answer.equals("3")){
+			;
+		}else if(answer.equals("4")){
+			;
+		}else if(answer.equals("5")){
+			;
+		}
+	}
+
+	public static void certainDesign(Scanner menuAns, Connection mySQLDB) throws SQLException{
+		//TODO
+	}
+
+	public static void bestDesign(Scanner menuAns, Connection mySQLDB) throws SQLException{
+		//TODO
+	}
+
+	public static void customerMenu(Scanner menuAns, Connection mySQLDB) throws SQLException{//done
+		String answer = "";
+		
+		while(true){
+			System.out.println();
+			System.out.println("-----Operations for exploration companies (rental customers)-----");
+			//System.out.println("What kinds of operation would you like to perform?");
+			System.out.println("1. Search for NEAs based on some criteria");
+			System.out.println("2. Search for spacecrafts based on some criteria");
+			System.out.println("3. A certain NEA exploration mission design");
+			System.out.println("4. The most beneficial NEA exploration mission design");
+			System.out.println("0. Return to the main menu");
+			System.out.print("Enter Your Choice: ");
+			answer = menuAns.nextLine();
+
+			if(answer.equals("1")){
+				neaSearch(menuAns, mySQLDB);
+			}else if(answer.equals("2")){
+				spacecraftSearch(menuAns, mySQLDB);
+			}else if(answer.equals("3")){
+				certainDesign(menuAns, mySQLDB);
+			}else if(answer.equals("4")){
+				bestDesign(menuAns, mySQLDB);
+			}else if(answer.equals("0")){
+				break;
+			}else{
+				System.out.println("[Error]: Wrong Input, Type in again!!!");
+			}
+		}	
+	}
+
+	public static void rentSpacecraft(Scanner menuAns, Connection mySQLDB) throws SQLException{
+		//TODO
+	}
+
+	public static void returnSpacecraft(Scanner menuAns, Connection mySQLDB) throws SQLException{
+		//TODO
+	}
+
+	public static void listRentedByTime(Scanner menuAns, Connection mySQLDB) throws SQLException{
+
+	}
+	public static void listRentedNum(Scanner menuAns, Connection mySQLDB) throws SQLException{
 
 	}
 
 	public static void staffMenu(Scanner menuAns, Connection mySQLDB) throws SQLException{
+		String answer = "";
+		
+		while(true){
+			System.out.println();
+			System.out.println("-----Operations for spacecraft rental staff-----");
+			//System.out.println("What kinds of operation would you like to perform?");
+			System.out.println("1. Rent a spacecraft");
+			System.out.println("2. Return a spacecraft");
+			System.out.println("3. List all the spacecraft currently rented out (on a mission) for a certain period");
+			System.out.println("4. List the number of spacecrafts currently rented out by each Agency");
+			System.out.println("0. Return to the main menu");
+			System.out.print("Enter Your Choice: ");
+			answer = menuAns.nextLine();
 
+			if(answer.equals("1")){
+				rentSpacecraft(menuAns, mySQLDB);
+			}else if(answer.equals("2")){
+				returnSpacecraft(menuAns, mySQLDB);
+			}else if(answer.equals("3")){
+				listRentedByTime(menuAns, mySQLDB);
+			}else if(answer.equals("4")){
+				listRentedNum(menuAns, mySQLDB);
+			}else if(answer.equals("0")){
+				break;
+			}else{
+				System.out.println("[Error]: Wrong Input, Type in again!!!");
+			}
+		}	
 	}
 
 	public static void main(String[] args) {
