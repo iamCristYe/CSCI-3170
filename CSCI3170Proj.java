dd5a6ffa8a6fb9aa213e37185896841509dc000f
zr67800 <17163263+zr67800@users.noreply.github.com>
2018-04-20 00:26:00 +0800

    bug fix

--- CSCI3170Proj.java
+++ CSCI3170Proj.java
@@ -421,7 +421,7 @@ public class CSCI3170Proj {
 		System.out.println("|Agency| MID|SNum|Type|Energy|  T|Capacity|Charge|");
 		ResultSet resultSet = stmt.executeQuery();
 		while (resultSet.next()) {
-			for (int i = 1; i <= 8; i++) {
+			
 				System.out.print("|" + String.format("%1$6s", resultSet.getString(1)));
 				System.out.print("|" + String.format("%1$4s", resultSet.getString(2)));
 				System.out.print("|" + String.format("%1$4s", resultSet.getString(3)));
@@ -431,7 +431,7 @@ public class CSCI3170Proj {
 				System.out.print("|" + String.format("%1$8s", resultSet.getString(7)));
 				System.out.print("|" + String.format("%1$6s", resultSet.getString(8)));
 				System.out.println("|");
-			}
+			
 		}
 		System.out.println("End of Query");
 		resultSet.close();
